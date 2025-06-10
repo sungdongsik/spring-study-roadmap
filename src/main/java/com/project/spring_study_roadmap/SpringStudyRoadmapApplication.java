06957e57f25ab2d906169214de4f7bff824d107c
성동식 <tjdehdtlr123@naver.com>
2025-06-10 16:04:09 +0900

    06-10(화) 스프링부트 셋팅

---
+++ src/main/java/com/project/spring_study_roadmap/SpringStudyRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring_study_roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyRoadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyRoadmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/spring_study_roadmap/SpringStudyRoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring_study_roadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyRoadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
