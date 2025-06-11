ddbf3044d7c8e39ea0ad01f2afef373355e52e98
성동식 <tjdehdtlr123@naver.com>
2025-06-12 01:30:20 +0900

    06-12(목) 스프링 DB 접근 기술(1)

--- src/main/java/com/project/spring_study_roadmap/controller/MemberController.java
+++ src/main/java/com/project/spring_study_roadmap/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.project.spring_study_roadmap.controller;
 
+import com.project.spring_study_roadmap.domain.Member;
 import com.project.spring_study_roadmap.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,6 +29,26 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String createForm(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 
 
+    @GetMapping("/members")
+    public String list(Model model){
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
+    }
 }
