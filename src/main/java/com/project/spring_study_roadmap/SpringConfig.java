9a0f199d66ce1d8a7355ce0c77e4e9506b66eb8e
성동식 <tjdehdtlr123@naver.com>
2025-06-12 01:56:49 +0900

    06-12(목) 스프링 DB 접근 기술(2)

--- src/main/java/com/project/spring_study_roadmap/SpringConfig.java
+++ src/main/java/com/project/spring_study_roadmap/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.project.spring_study_roadmap;
 
+import com.project.spring_study_roadmap.repository.JdbcMemberRepository;
 import com.project.spring_study_roadmap.repository.MemberRepository;
 import com.project.spring_study_roadmap.repository.MemoryMemberRepository;
 import com.project.spring_study_roadmap.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
